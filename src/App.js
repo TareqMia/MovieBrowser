2a6c8946fe3eb32605c03a32482934e6769cdc5a
Tareq Mia <tareqmia81@gmail.com>
2021-06-20 20:24:24 -0400

    fixing mistake

--- src/App.js
+++ src/App.js
@@ -12,7 +12,8 @@ import axios from 'axios';
  */
 const App = () => {
   const apikey = `10abef78`;
-  const url = `https://://www.omdbapi.com/?apikey=${apikey}&`;
+  const url = `https://www.omdbapi.com/?apikey=${apikey}&`;
+  
 
   /**
    * setting up state using useState hook
